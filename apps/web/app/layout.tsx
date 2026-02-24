d27e0b2a7e85b38de4e018267f775278313f839a
Ninad09 <ninadgaonkar234@gmail.com>
2026-02-24 23:54:59 +0530

    feat: add tanstack provider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google"
 import "./globals.css";
+import Providers from "@/components/provider";
 
 const manrope = Manrope({})
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${manrope.className} `}>
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
