d22f584e1c656652ae0a8c5c0d346e50598c024f
Ninad09 <ninadgaonkar234@gmail.com>
2026-02-24 23:52:14 +0530

    refactor: move to server shell architecture

--- apps/web/app/monitor/[id]/page.tsx
+++ apps/web/app/monitor/[id]/page.tsx
@@ -1,32 +1,8 @@
-import { getMonitorData } from "@/lib/getMonitorData"
-import { redirect } from "next/navigation"
-import MonitorHeader from "./_components/monitorHeader"
-import RegionCards from "./_components/regionCards"
-import RegionalLatency from "./_components/regionLatencyGraph"
-import UptimeOverview from "./_components/uptimeOverview"
-import IncidentTimeline from "./_components/incidentList"
-import LatencyTrend from "./_components/latencyTrend"
+import MonitorDashboard from "./_components/monitorDashboard"
 
-
-
-export default async function MonitorPage({params}: { params: Promise<{ id: string}> }) {
-    const websiteId = (await params).id
-    const data = await getMonitorData(websiteId)
+export default async function MonitorPage({ params }: { params: Promise<{ id : string}>}) {
     
-    if (!data) {
-        redirect('/dashboard')
-    }
+  const { id } = await params
 
-    return (
-        <div className="bg-gray-50/50">
-            <div className="flex flex-col mx-auto h-full max-w-5xl bg-gray-50 border">
-                <MonitorHeader data={data} />
-                <RegionCards data={data} />
-                <RegionalLatency data={data} />
-                <LatencyTrend data={data} />
-                <UptimeOverview data={data}/>
-                <IncidentTimeline data={data}/>
-            </div>
-        </div>
-    )
+  return <MonitorDashboard id={id} />
 }
