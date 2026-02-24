7b92af2df1bf8e952b3d508a8900c0590d0b425d
Ninad09 <ninadgaonkar234@gmail.com>
2026-02-24 23:57:32 +0530

    refactor: remove unstable cache

--- apps/web/lib/getDashboardData.ts
+++ apps/web/lib/getDashboardData.ts
@@ -1,47 +1,44 @@
 import { prisma } from "@repo/db/client"
-import { unstable_cache } from "next/cache"
 
-export const getDashboardData = unstable_cache(
-  async (userId: string) => {
-    
-    const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000)
+export const getDashboardData = async (userId: string) => {
+  const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000)
 
-    const websites = await prisma.website.findMany({
-      where: { userId },
-      select: {
-        id: true,
-        name: true,
-        url: true,
-        currentStatus: true,
-        lastChecked: true,
-        incidents: {
-          where: { endedAt: null },
-          take: 1,
-          select: { id: true }
-        }
+  const websites = await prisma.website.findMany({
+    where: { userId },
+    select: {
+      id: true,
+      name: true,
+      url: true,
+      currentStatus: true,
+      lastChecked: true,
+      incidents: {
+        where: { endedAt: null },
+        take: 1,
+        select: { id: true }
       }
-    })
+    }
+  })
 
-    const metrics = await prisma.websiteMetric.groupBy({
-      by: ["websiteId"],
-      where: { windowStart: { gte: oneDayAgo } },
-      _avg: {
-        uptimePercent: true,
-        avgResponseTimeMs: true
-      }
-    })
+  const metrics = await prisma.websiteMetric.groupBy({
+    by: ["websiteId"],
+    where: { windowStart: { gte: oneDayAgo } },
+    _avg: {
+      uptimePercent: true,
+      avgResponseTimeMs: true
+    }
+  })
 
-    const metricsMap = new Map(
-      metrics.map(m => [m.websiteId, m])
-    )
+  const metricsMap = new Map(
+    metrics.map(m => [m.websiteId, m])
+  )
 
-    return websites.map(site => {
-      const m = metricsMap.get(site.id)
+  return websites.map(site => {
+    const m = metricsMap.get(site.id)
 
-      return {
-        ...site,
-        uptime24h: m?._avg.uptimePercent ?? 100,
-        avgResponseTime: m?._avg.avgResponseTimeMs ?? null
-      }
-    })
-})
+    return {
+      ...site,
+      uptime24h: m?._avg.uptimePercent ?? 100,
+      avgResponseTime: m?._avg.avgResponseTimeMs ?? null
+    }
+  })
+}
