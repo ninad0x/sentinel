901409a8683c9a9fcc1d5612fff77649243cd613
Ninad09 <ninadgaonkar234@gmail.com>
2026-04-05 12:26:24 +0530

    change button bg-color

--- apps/web/components/heroNav.tsx
+++ apps/web/components/heroNav.tsx
@@ -29,7 +29,7 @@ export function Header() {
 
           <Link prefetch={true} 
             href="/sign-in"
-            className="px-4 py-2 bg-gray-900 text-white text-sm font-medium rounded-lg hover:bg-gray-800 transition"
+            className="px-4 py-2 bg-neutral-800 text-white text-sm font-medium rounded-lg hover:bg-neutral-700 transition"
           >
             Sign In
           </Link>
